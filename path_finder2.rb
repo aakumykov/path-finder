d192e9412459ce70881ea51f7536d7b5d735972e
Андрей Кумыков <aakumykov@yandex.ru>
2016-05-06 19:27:26 +0700

    продвигаю Следопыта-2

--- path_finder2.rb
+++ path_finder2.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/env ruby
 # coding: utf-8
 require 'rmagick'
-require 'mathn'
 include Magick
 
 class PathFinder
@@ -10,21 +9,18 @@ class PathFinder
 	end
 
 	def find_path(start_x, start_y, opt={})
-		# настройка пераметров
-		@search_radius = opt.fetch(:radius,10)
-		
-		# рассчёт шаблона лучей
-		@rays_template = calc_rays_template()
+		# предварительная настройка
+		search_radius = opt.fetch(:radius,10)
+		@rays_template = calc_rays_template(search_radius)
 
 		# получение массива шагов
 		all_steps = []
 		
 		step = get_step(start_x,start_y)
-		all_steps << [step[:x],step[:y]]
+		all_steps << step
 		
-		while step = get_step(step[:x],step[:y])
-			all_steps << [step[:x],step[:y]]
-			break if (step[:x]==0 || step[:y]==0)
+		while step = get_step(step[0],step[1])
+			all_steps << step
 		end
 		
 		return all_steps
@@ -32,12 +28,68 @@ class PathFinder
 
 	private
 
-		def calc_rays_template()
-			
+		def calc_rays_template(radius)
+			data = [
+				[ [rand(@img.columns), rand(@img.rows)], [rand(@img.columns), rand(@img.rows)], [rand(@img.columns), rand(@img.rows)], [rand(@img.columns), rand(@img.rows)], [rand(@img.columns), rand(@img.rows)] ],
+				[ [rand(@img.columns), rand(@img.rows)], [rand(@img.columns), rand(@img.rows)], [rand(@img.columns), rand(@img.rows)], [rand(@img.columns), rand(@img.rows)], [rand(@img.columns), rand(@img.rows)] ],
+				[ [rand(@img.columns), rand(@img.rows)], [rand(@img.columns), rand(@img.rows)], [rand(@img.columns), rand(@img.rows)], [rand(@img.columns), rand(@img.rows)], [rand(@img.columns), rand(@img.rows)] ],
+
+				# [ [1,1], [1,2], [1,3], [1,4], [1,5], ],
+				# [ [2,1], [2,2], [2,3], [2,4], [2,5], ],
+				# [ [3,1], [3,2], [3,3], [3,4], [3,5], ],
+			]
+			#puts data.inspect
+			#puts data.transpose.inspect
+			data.transpose
 		end
 
 		def get_step(x,y)
-			{ x: rand(5), y: rand(5) }
+			puts "#{__method__}(#{x},#{y})"
+
+			# получаю данные всех лучей
+			all_rays = get_all_rays(x,y)
+			
+			#puts "all_rays: #{all_rays}"
+
+			# нахожу "лучший"
+			detect_best_ray(all_rays)
+		end
+
+		def get_all_rays(x0,y0)
+			puts "#{__method__}(#{x0},#{y0})"
+			
+			rays = []
+			
+			one_ray = []
+			for ray in @rays_template do
+				for ray_dot in ray do
+					x = x0+ray_dot[0]
+					y = y0+ray_dot[1]
+					#puts "ray_dot: #{ray_dot}, #{x}, #{y}"
+					
+					pix = @img.get_pixels(x,y,1,1).first
+					ray_data = pix.red + pix.green + pix.blue
+					one_ray << ray_data
+				end
+				rays << one_ray
+				one_ray = []
+			end
+
+			return rays
+		end
+
+		def detect_best_ray(rays_bunch)
+			puts "#{__method__}(#{rays_bunch})"
+
+			all_rays = []
+
+			rays_bunch.each { |ray|
+				all_rays << ray.inject(0){|sum,x| sum + x }
+			}
+
+			puts all_rays.inspect
+
+			[ all_rays.max, all_rays.min ]
 		end
 end
 
@@ -50,5 +102,5 @@ else
 end
 
 pf = PathFinder.new(img_file)
-path = pf.find_path(10,10)
+path = pf.find_path(0,0)
 puts path.inspect
