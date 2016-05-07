33a966d797056fef9344749f94a03e41de1831e5
Андрей Кумыков <aakumykov@yandex.ru>
2016-05-08 00:36:22 +0700

    Насотящий шаблон лучей. Ошибки логики

--- path_finder2.rb
+++ path_finder2.rb
@@ -3,9 +3,12 @@
 require 'rmagick'
 include Magick
 
+
 class PathFinder
 	def initialize(img_file)
 		@img = ImageList.new(img_file).first
+		puts "image width: #{@img.columns}"
+		puts "image height: #{@img.rows}"
 	end
 
 	def find_path(start_x, start_y, opt={})
@@ -14,12 +17,12 @@ class PathFinder
 		# предварительная настройка
 		search_radius = opt.fetch(:radius,10)
 		@rays_template = calc_rays_template(search_radius)
+		#puts @rays_template.inspect; exit
 
 		# получение массива шагов
 		x,y = start_x, start_y
 		all_steps = []
 		
-		puts ''
 		while step = get_step(x,y)
 			puts "step: #{step}"
 			all_steps << step
@@ -32,19 +35,80 @@ class PathFinder
 
 	private
 
-		def calc_rays_template(radius)
-			data = [
-				[ [rand(@img.columns), rand(@img.rows)], [rand(@img.columns), rand(@img.rows)], [rand(@img.columns), rand(@img.rows)], [rand(@img.columns), rand(@img.rows)], [rand(@img.columns), rand(@img.rows)] ],
-				[ [rand(@img.columns), rand(@img.rows)], [rand(@img.columns), rand(@img.rows)], [rand(@img.columns), rand(@img.rows)], [rand(@img.columns), rand(@img.rows)], [rand(@img.columns), rand(@img.rows)] ],
-				[ [rand(@img.columns), rand(@img.rows)], [rand(@img.columns), rand(@img.rows)], [rand(@img.columns), rand(@img.rows)], [rand(@img.columns), rand(@img.rows)], [rand(@img.columns), rand(@img.rows)] ],
+		def calc_rays_template(radius,opt={})
+			
+			# параметры
+			angle_start = opt.fetch(:angle_start,0)
+			angle_end = opt.fetch(:angle_end,359)
 
-				# [ [1,1], [1,2], [1,3], [1,4], [1,5], ],
-				# [ [2,1], [2,2], [2,3], [2,4], [2,5], ],
-				# [ [3,1], [3,2], [3,3], [3,4], [3,5], ],
-			]
-			#puts data.inspect
-			#puts data.transpose.inspect
-			data.transpose
+			dA = 360/(radius*2)
+
+			# служебные функции
+			def g2r(n)
+				n*(Math::PI/180)
+			end
+
+			def r2g(n)
+				n*(180/Math::PI)
+			end
+
+			def fix_signs(angle,x,y)
+				angle = angle.to_f % 360
+
+				def make_positive(arg)
+					arg = arg.to_f
+					arg < 0 ? arg*(-1) : arg
+				end
+				
+				def make_negative(arg)
+					arg = arg.to_f
+					arg < 0 ? arg : arg*(-1)
+				end
+
+				case angle
+				when 0...90
+					[ make_positive(x), make_positive(y) ]
+				when 90...180
+					[ make_negative(x), make_positive(y) ]
+				when 180...270
+					[ make_negative(x), make_negative(y) ]
+				when 270...360
+					[ make_positive(x), make_negative(y) ]
+				else
+					raise "out of range (#{angle})"
+				end
+			end
+
+			# вычисления
+			all_rays = []
+
+			a = angle_start
+			r = 1
+			
+			while r <= radius
+				ray = []
+
+				while a <= angle_end do
+					begin
+						k = Math.tan( g2r(a) )
+						x = Math.sqrt( r**2 / (k**2 + 1) )
+						y = k*x
+
+						ray << fix_signs(a,x,y)
+					rescue
+						puts "НЕТ ТАНГЕНСА ДЛЯ #{a}"
+					ensure
+						a += dA
+					end
+				end
+
+				all_rays << ray
+
+				a = angle_start
+				r += 1
+			end
+
+			all_rays.transpose
 		end
 
 		def get_step(x,y)
@@ -52,28 +116,31 @@ class PathFinder
 
 			# получаю данные всех лучей
 			all_rays = get_all_rays(x,y)
-			#puts "all_rays: #{all_rays}"
+			puts "all_rays:"; all_rays.each { |r| puts "#{r}" }
 
 			# нахожу "лучший"
 			best_ray = detect_best_ray(all_rays)
-			#puts "best_ray: #{best_ray}"
+			puts "best_ray: #{best_ray}"
+			exit
 
 			return best_ray
 		end
 
 		# возвращает массив хэшей
 		def get_all_rays(x0,y0)
-			#puts "#{__method__}(#{x0},#{y0})"
+			puts "#{__method__}(#{x0},#{y0})"
 			
 			rays = []
 			
 			for one_ray in @rays_template do
+				#puts "one_ray: #{one_ray}"
 				
 				for ray_dot in one_ray do
-				
+
 					x = x0+ray_dot[0]
 					y = y0+ray_dot[1]
-					puts "ray_dot: #{ray_dot}, #{x}, #{y}"
+					
+					# puts "RAY DOT: template: #{ray_dot}, real: #{x}, #{y}"
 					
 					pix = @img.get_pixels(x,y,1,1).first
 					
@@ -106,5 +173,5 @@ else
 end
 
 pf = PathFinder.new(img_file)
-path = pf.find_path(0,0)
+path = pf.find_path(9,9)
 puts "path: #{path.inspect}"
